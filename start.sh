8b64ed3fd56f01fd1c4893160be61c7793827712
laobiz <pothjonas@gmail.com>
2020-05-13 09:33:13 +0200

    fix

--- start.sh
+++ start.sh
@@ -6,7 +6,10 @@ cachix authtoken eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1JDU4MDAiLCJqdGkiOiI2ODI2MzM3Ny
 cachix use digitallyinduced
 
 # Unless the RunDevServer binary is available, we rebuild the .envrc cache with nix-shell
-command -v RunDevServer >/dev/null 2>&1 || { echo "PATH_add $(nix-shell --run 'echo $PATH')" > .envrc; }
+command -v RunDevServer >/dev/null 2>&1 \
+    || { cachix authtoken eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1JDU4MDAiLCJqdGkiOiI2ODI2MzM3Ny04ZjA0LTQzNjQtOTc5Zi0yMTdkNDg2MGFjODciLCJzY29wZXMiOiJ0eCJ9.nb37o2CQBt1Fz3MBMppNACdaWDQsjbPvYpPMLgeJ7RI; \
+         cachix use digitallyinduced; \
+         echo "PATH_add $(nix-shell --run 'echo $PATH')" > .envrc; }
 
 # Now we have to load the PATH variable from the .envrc cache
 direnv allow
