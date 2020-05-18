3101bde776f261601bfd67c00d896ea0c40d342c
laobiz <pothjonas@gmail.com>
2020-05-18 08:54:26 +0200

    further

--- default.nix
+++ default.nix
@@ -12,7 +12,7 @@ let
             wai
             text
             hlint
-            turbohaskell
+            ihp
         ];
         otherDeps = p: with p; [
             # Native dependencies, e.g. imagemagick
