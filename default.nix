c487efda30c47b869bc9db23f88880f9aedf7893
Marc Scholten <marc@digitallyinduced.com>
2020-05-23 17:41:52 +0200

    Updated framework version

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 let
     ihp = builtins.fetchGit {
         url = "https://github.com/digitallyinduced/haskellframework.git";
-        rev = "d2bca51aa03681eaad758b0097a67e45ad7bf574";
+        rev = "4a8ac0e7a6bd67da59ea573dd11ce84a893e3223";
     };
     haskellEnv = import "${ihp}/NixSupport/default.nix" {
         ihp = ihp;
