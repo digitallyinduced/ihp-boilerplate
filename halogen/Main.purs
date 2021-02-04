65c86a8f4dfaeb4442fe9c75b84ef503b13c709a
Daniel Sokil <dtechtours@gmail.com>
2021-02-04 14:33:46 -0500

    Updates With esbuild Bundler

--- halogen/Main.purs
+++ halogen/Main.purs
@@ -19,13 +19,13 @@ component =
   H.mkComponent
     { initialState: const unit
     , eval: H.mkEval H.defaultEval
-    , render: const $ HH.div_ [ HH.text "PureScript + Halogen works great!" ]
+    , render: const $ HH.div_ [ HH.h1_ [ HH.text "PureScript + Halogen is also working!" ] ]
     }
 
 main :: Effect Unit
 main =
   runHalogenAff do
-    rootElement <- selectElement' "PureScript Halogen could not find element 'div.halogen'" $ QuerySelector "div.halogen"
+    rootElement <- selectElement' "PureScript Halogen could not find element '#halogen'" $ QuerySelector "#halogen"
     (runUI component unit) rootElement
 
 selectElement' :: String -> QuerySelector -> Aff HTMLElement
