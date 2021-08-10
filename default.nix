b0fd7fd2461a7c86cdcda34f8279b18a39834391
Marc Scholten <marc@digitallyinduced.com>
2021-08-10 10:46:34 +0200

    Upgrade to IHP v0.13.1

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 let
     ihp = builtins.fetchGit {
         url = "https://github.com/digitallyinduced/ihp.git";
-        ref = "refs/tags/v0.13.0";
+        ref = "refs/tags/v0.13.1";
     };
     haskellEnv = import "${ihp}/NixSupport/default.nix" {
         ihp = ihp;
