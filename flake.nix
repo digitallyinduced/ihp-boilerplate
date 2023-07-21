8f42f187b69b86ed54377c1aeed580ddf25f16ed
Marc Scholten <marc@digitallyinduced.com>
2023-07-21 09:28:16 +0200

    Use IHP v1.1 branch

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
     inputs = {
-        ihp.url = "github:digitallyinduced/ihp";
+        ihp.url = "github:digitallyinduced/ihp/v1.1";
         nixpkgs.follows = "ihp/nixpkgs";
         flake-parts.follows = "ihp/flake-parts";
         devenv.follows = "ihp/devenv";
