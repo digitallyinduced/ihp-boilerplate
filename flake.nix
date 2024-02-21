d8dc79c21147a6ff2c80e422d4938ad38d6a26b6
Amitai Burstein <amitai@gizra.com>
2024-02-21 20:19:40 +0200

    Add more defaults an docs to flake.nix (#30)
    * Add more defaults an docs to flake.nix
    
    * SMTP config for local development.
    
    * Code review fixes
    
    * Change app name to `qa`
    
    * Remove mailhog from packages
    
    * Add SMTP_ENCRYPTION
    
    * envName
    
    * Update flake.nix
    
    * Update flake.nix
    
    * Apply suggestions from code review
    
    Co-authored-by: Marc Scholten <marc@digitallyinduced.com>
    
    * Apply suggestions from code review
    
    ---------
    
    Co-authored-by: Marc Scholten <marc@digitallyinduced.com>

--- flake.nix
+++ flake.nix
@@ -27,8 +27,85 @@
                         base
                         wai
                         text
+
+                        # Uncomment on local development for testing
+                        # hspec
                     ];
                 };
+
+                # Custom configuration that will start with `devenv up`
+                devenv.shells.default = {
+                    # Start Mailhog on local development to catch outgoing emails
+                    # services.mailhog.enable = true;
+
+                    # Custom processes that don't appear in https://devenv.sh/reference/options/
+                    processes = {
+                        # Uncomment if you use tailwindcss.
+                        # tailwind.exec = "tailwindcss -c tailwind/tailwind.config.js -i ./tailwind/app.css -o static/app.css --watch=always";
+                    };
+                };
+            };
+
+            # Adding the new NixOS configuration for "qa"
+            # See https://ihp.digitallyinduced.com/Guide/deployment.html#deploying-with-deploytonixos for more info
+            # Used to deploy the IHP application to AWS.
+            #
+            # Change the `CHANGE-ME` to your correct config.
+            flake.nixosConfigurations."qa" = nixpkgs.lib.nixosSystem {
+                system = "x86_64-linux";
+                specialArgs = inputs;
+                modules = [
+                    "${nixpkgs}/nixos/modules/virtualisation/amazon-image.nix"
+                    ihp.nixosModules.appWithPostgres
+                    ({ lib, pkgs, ... }: {
+
+                        networking.firewall = {
+                            enable = true;
+                            allowedTCPPorts = [ 22 80 443 ];
+                        };
+
+                        # Enable the Let's encrypt certificate
+                        security.acme.defaults.email = "CHANGE-ME@example.com";
+
+                        # Accept the terms of service of the Let's encrypt provider.
+                        security.acme.acceptTerms = true;
+
+                        services.nginx = {
+                            virtualHosts."CHANGE-ME.com" =  {
+                                # Uncomment to have http auth with username `foo` and password `bar`.
+                                # basicAuth = { foo = "bar"; };
+                            };
+                        };
+
+                        services.ihp = {
+                            domain = "CHANGE-ME.com";
+                            migrations = ./Application/Migration;
+                            schema = ./Application/Schema.sql;
+                            fixtures = ./Application/Fixtures.sql;
+                            sessionSecret = "CHANGE-ME";
+                            additionalEnvVars = {
+                                # Uncomment to use a custom database URL
+                                # DATABASE_URL = "postgresql://postgres:...CHANGE-ME";
+
+                                SMTP_HOST = "email-smtp.eu-west-1.amazonaws.com";
+                                SMTP_PORT = "587";
+                                SMTP_ENCRYPTION = "STARTTLS";
+
+                                SMTP_USER = "CHANGE-ME";
+                                SMTP_PASSWORD = "CHANGE-ME";
+
+                                AWS_ACCESS_KEY_ID = "CHANGE-ME";
+                                AWS_SECRET_ACCESS_KEY = "CHANGE-ME";
+                            };
+                        };
+                        # As we use a pre-built AMI on AWS,
+                        # it is essential to enable automatic updates.
+                        # @see https://nixos.wiki/wiki/Automatic_system_upgrades
+                        system.autoUpgrade.enable = true;
+                        # Keep as is. See https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+                        system.stateVersion = "23.05";
+                    })
+                ];
             };
 
         };
