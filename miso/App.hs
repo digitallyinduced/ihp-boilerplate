1e98d3ed60aafe00507bb1549d86690edf5d1f3c
George Thomas <georgefsthomas@gmail.com>
2025-02-19 15:59:46 +0000

    Add placeholder text for Miso frontend

--- miso/App.hs
+++ miso/App.hs
@@ -1,9 +1,22 @@
 module App (start) where
 
-import Miso
+import Control.Monad (void)
+import Control.Monad.Trans.Maybe (MaybeT (MaybeT, runMaybeT))
+import JSDOM (currentDocument)
+import JSDOM.Generated.Node (removeChild)
+import JSDOM.Generated.NonElementParentNode (getElementById)
+import JSDOM.Generated.ParentNode (getFirstElementChild)
+import Miso hiding (getElementById)
+import Miso.String (MisoString)
 
 start :: JSM ()
-start = startApp app
+start = do
+    maybe (consoleLog "placeholder node not found") (void . uncurry removeChild) =<< runMaybeT do
+        doc <- MaybeT currentDocument
+        parent <- MaybeT $ getElementById doc ("miso" :: MisoString)
+        child <- MaybeT $ getFirstElementChild parent
+        pure (parent, child)
+    startApp app
 
 app :: App Model Action
 app = App
