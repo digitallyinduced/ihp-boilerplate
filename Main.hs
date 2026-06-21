9af554bfeea433ba7aca5334b5507b45158754f1
Marc Scholten <marc@digitallyinduced.com>
2026-06-21 05:25:01 +0200

    Prepare boilerplate for IHP v1.6

--- Main.hs
+++ Main.hs
@@ -5,13 +5,9 @@ import Config
 import qualified IHP.Server
 import IHP.RouterSupport
 import IHP.FrameworkConfig
-import IHP.Job.Types
 
 instance FrontController RootApplication where
     controllers = []
-
-instance Worker RootApplication where
-    workers _ = []
 
 main :: IO ()
 main = IHP.Server.run config
